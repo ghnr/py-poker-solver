1b653fc4bc4d04f9340218f44d945481712c0a79
ghnr <45798027+ghnr@users.noreply.github.com>
2018-12-12 18:41:43 +0000

    remove Q-Learning code

--- learning.py
+++ learning.py
@@ -347,53 +347,6 @@ class PokerGame:
         return reward
 
 
-# class QLearningTable:
-#     def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
-#         # QLearning experiment, modified code from online
-#         # Not feasible
-#         self.actions = actions  # a list
-#         self.lr = learning_rate
-#         self.gamma = reward_decay
-#         self.epsilon = e_greedy
-#         self.q_table = pd.DataFrame(columns=self.actions)
-#
-#     def load(self):
-#         try:
-#             self.q_table = pd.read_pickle('data.pkl')
-#         except FileNotFoundError:
-#             print("File not found")
-#
-#     def choose_action(self, observation):
-#         self.check_state_exist(observation)
-#         # action selection
-#         if np.random.uniform() < self.epsilon:
-#             # choose best action
-#             state_action = self.q_table.ix[observation, :]
-#             state_action = state_action.reindex(np.random.permutation(state_action.index))     # some actions have same value
-#             action = state_action.argmax()
-#         else:
-#             # choose random action
-#             action = np.random.choice(self.actions)
-#         return action
-#
-#     def learn(self, s, a, r, s_):
-#         self.check_state_exist(s_)
-#         q_predict = self.q_table.ix[s, a]
-#         if s_ != 'terminal':
-#             q_target = r + self.gamma * self.q_table.ix[s_, :].max()  # next state is not terminal
-#         else:
-#             q_target = r  # next state is terminal
-#         self.q_table.ix[s, a] += self.lr * (q_target - q_predict)  # update
-#         print(self.q_table.head(1))
-#
-#     def check_state_exist(self, state):
-#         if state not in self.q_table.index:
-#             # append new state to q table
-#             self.q_table = self.q_table.append(pd.Series([0]*len(self.actions),index=self.q_table.columns,name=state,))
-#
-#     def save(self):
-#         self.q_table.to_pickle('data.pkl')
-
 def readable(hand):
     return [CARDS_REVERSE[x] for x in hand]
 
